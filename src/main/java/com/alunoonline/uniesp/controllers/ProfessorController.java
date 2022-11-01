099c905a81a27ed5ba9d4fa712597ca9b77e3eb5
José Roberto <joserobertodsj@gmail.com>
2022-11-01 14:57:54 -0300

    Subindo Professor - Método buscar todos

--- src/main/java/com/alunoonline/uniesp/controllers/ProfessorController.java
+++ src/main/java/com/alunoonline/uniesp/controllers/ProfessorController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/professores")
@@ -29,6 +27,10 @@ public class ProfessorController {
         var professorModel = new ProfessorModel();
         BeanUtils.copyProperties(professorDto, professorModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(professorService.save(professorModel));
+    }
 
+    @GetMapping
+    public ResponseEntity<List<ProfessorModel>> buscarTodos(){
+        return ResponseEntity.status(HttpStatus.OK).body(professorService.buscarTodos());
     }
 }
--- src/main/java/com/alunoonline/uniesp/services/ProfessorService.java
+++ src/main/java/com/alunoonline/uniesp/services/ProfessorService.java
@@ -6,13 +6,21 @@ import com.alunoonline.uniesp.repositories.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProfessorService {
+
 
     @Autowired
     private ProfessorRepository professorRepository;
 
     public Object save(ProfessorModel professorModel) {
         return professorRepository.save(professorModel);
+    }
+
+
+    public List<ProfessorModel> buscarTodos() {
+        return professorRepository.findAll();
     }
 }
